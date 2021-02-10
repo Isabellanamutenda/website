da590917578235e9ba1193318290859c8d0deb56
Isabellanamutenda <namtendaisabella@.com>
2021-02-11 01:06:50 +0300

    first commit

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
         if(this.scrollY > 500){
             $(".scroll-up-btn").addClass("show");
         }else{
-            $(".scroll-up-btn").removeClass("sticky");
+            $(".scroll-up-btn").removeClass("show");
         }
     });
 
@@ -26,10 +26,10 @@ $(document).ready(function(){
 });
 
 // typing animation script
-var typed = new Typed(".typing", {
-    strings: ["Software", "Developer"],
-    typeSpeed: 100,
-    backspeed: 60,
-    loop: true
+// var typed = new Typed(".typing", {
+//     strings: ["Software", "Developer"],
+//     typeSpeed: 100,
+//     backSpeed: 60,
+//     loop: true
 
-});
+// });
